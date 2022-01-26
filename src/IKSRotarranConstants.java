fb03c6dc75bead54c04a1b1825cabdcdea6cb211
leblair <bryan.a.a7@gmail.com>
2022-01-26 19:22:30 +0100

    Initial commit

---
+++ src/IKSRotarranConstants.java
@@ -0,0 +1,9 @@
+public interface IKSRotarranConstants {
+	String[] LLOCS_DE_SERVEI = {"", "pont", "enginyeria", "cuina", "infermeria", "sala d'armes"};
+	String[] DEPARTAMENTS = {"", "Comandament", "Armes", "Timó i navegació", "Enginyeria", "Ciència"};
+	
+	String pathBD = "baseDeDades/Rotarran_logistica.db4o";
+	
+	String pathArxiusExportacioXML = "arxiusXML/productesExportats.xml";
+	String pathArxiusImportacioXML = "arxiusXML/productesImportats.xml";
+}
